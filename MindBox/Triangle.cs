eecf8da6ed0104c8afa6490925efccb2cd726870
neverduda <30334339+neverduda@users.noreply.github.com>
2017-07-21 19:48:41 +0300

    Add files via upload

---
+++ MindBox/Triangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MindBox
+{
+    public static class Triangle
+    {
+        public static double СalcArea(double a, double b, double c)
+        {
+            if (!isItTriangle(a, b, c))
+                throw new ArgumentOutOfRangeException("Это не треугольник! Проверьте парамметры.");
+
+            if (!isItRightTriangle(a, b, c))
+                throw new ArgumentOutOfRangeException("Это не прямоугольный треугольник! Проверьте парамметры.");
+
+            return calcAreaByGeron(a, b, c);
+        }
+
+        /// <summary>
+        /// проверка корректности введенных параметров
+        /// </summary>
+        /// <param name="a">сторона 1</param>
+        /// <param name="b">сторона 2</param>
+        /// <param name="c">сторона 3</param>
+        /// <returns></returns>
+        private static bool isItTriangle(double a, double b, double c)
+        {
+            return ((a + b >= c) && (a + c >= b) && (c + b >= a) && a >= 0 && b >= 0 && c >= 0);
+        }
+
+        /// <summary>
+        /// Возвращает true, если треугольник прямоугольный
+        /// </summary>
+        /// <param name="a">сторона 1</param>
+        /// <param name="b">сторона 2</param>
+        /// <param name="c">сторона 3</param>
+        /// <returns></returns>
+        private static bool isItRightTriangle(double a, double b, double c)
+        {
+            double[] sides = new double[] { a, b, c };
+            Array.Sort(sides);
+            return (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2));
+        }
+
+
+        /// <summary>
+        /// Вычисляет площадь по формуле Герона
+        /// </summary>
+        /// <param name="a">сторона 1</param>
+        /// <param name="b">сторона 2</param>
+        /// <param name="c">сторона 3</param>
+        /// <returns></returns>
+        private static double calcAreaByGeron(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+        }
+    }
+}
---
+++ TestMindBox/TriangleTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MindBox;
+
+namespace TestMindBox
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        /// <summary>
+        /// Несуществующий треугольник
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestWrongParameters()
+        {
+            Triangle.СalcArea(1, 2, 1);
+        }
+
+        /// <summary>
+        /// Отрицательные параметры
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMinusParameters()
+        {
+            Triangle.СalcArea(1, -1, 1);
+        }
+
+        /// <summary>
+        /// не прямоугольный треугольник
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestNotRightTriangle()
+        {
+            Triangle.СalcArea(1, 1, 1);
+        }
+
+        /// <summary>
+        /// ОК
+        /// </summary>
+        [TestMethod]
+        public void TestCorrectTriangle()
+        {
+            Triangle.СalcArea(8, 15, 17);
+        }
+    }
+}
